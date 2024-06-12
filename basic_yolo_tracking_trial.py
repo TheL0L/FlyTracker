1530c1e19bff8d966d35b27c75a8de2f4a29e221
Sergei <sergeiyakima1@gmail.com>
2024-06-12 16:18:21 +0300

    tried the built-in tracking module of yolo, the results are inferior to the current solution

---
+++ basic_yolo_tracking_trial.py
@@ -0,0 +1,68 @@
+from ultralytics import YOLO
+import torch, os
+
+from collections import defaultdict
+import cv2
+import numpy as np
+
+_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = YOLO('./runs/detect/train7/weights/best.pt')
+model.to(_device)
+
+video_path = './showcase/2.avi'
+output_root_path = os.path.normpath('./showcase/result')
+_prefix = ''
+_suffix = '_result'
+
+file_name = os.path.basename(video_path)
+file_name = f'{_prefix}{file_name}{_suffix}'
+output_path = os.path.join(output_root_path, file_name)
+
+
+cap = cv2.VideoCapture(video_path)
+
+# Store the track history
+track_history = defaultdict(lambda: [])
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Get the boxes and track IDs
+        boxes = results[0].boxes.xywh.cpu()
+        track_ids = results[0].boxes.id.int().cpu().tolist()
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Plot the tracks
+        for box, track_id in zip(boxes, track_ids):
+            x, y, w, h = box
+            track = track_history[track_id]
+            track.append((float(x), float(y)))  # x, y center point
+            if len(track) > 100:  # retain 90 tracks for 90 frames
+                track.pop(0)
+
+            # Draw the tracking lines
+            points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=1)
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()

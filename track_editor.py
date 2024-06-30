c98c0c76c09c90c2718a1d69ac15d27390333678
Sergei <sergeiyakima1@gmail.com>
2024-06-30 23:34:33 +0300

    setup a basic gui layout for the tracks editor tool

---
+++ track_editor.py
@@ -0,0 +1,218 @@
+import tkinter as tk
+from tkinter import ttk
+import numpy as np
+import cv2
+from PIL import Image, ImageTk
+import storage_helper
+import data_postprocess
+import video_postprocess
+
+__INPUT_VIDEO = './showcase/4_short.avi'
+
+__BACKGROUND_DARK  = '#1E1E1E'
+__BACKGROUND_LIGHT = '#303030'
+
+
+def deselect_all_entries(event):
+    widget = event.widget
+    if not isinstance(widget, tk.Entry):
+        ROOT_WINDOW.focus()
+
+def restart_preview():
+    global VIDEO_CAPTURE, timeline_slider
+    VIDEO_CAPTURE.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    timeline_slider.set(VIDEO_CAPTURE.get(cv2.CAP_PROP_POS_FRAMES))
+
+def resume_preview():
+    global IS_PLAYING
+    IS_PLAYING = True
+
+def pause_preview():
+    global IS_PLAYING
+    IS_PLAYING = False
+
+def read_zoom_from_controls():
+    global ZOOM_SCALAR
+
+    ZOOM_SCALAR = zoom_slider.get() / 100
+
+def read_speed_from_controls():
+    global PLAYBACK_DELAY_MS
+
+    speed_factor = speed_slider.get() / 100
+    PLAYBACK_DELAY_MS = int(1000 / (VIDEO_CAPTURE.get(cv2.CAP_PROP_FPS) * speed_factor))
+
+def read_timeline_value():
+    global VIDEO_CAPTURE, timeline_slider
+
+    timeline_pos = int(timeline_slider.get())
+    if timeline_pos != int(VIDEO_CAPTURE.get(cv2.CAP_PROP_POS_FRAMES) - 1):
+        VIDEO_CAPTURE.set(cv2.CAP_PROP_POS_FRAMES, timeline_pos)
+
+def update_frame():
+    global VIDEO_CAPTURE, preview_panel, merged_frame, timeline_slider
+
+    read_timeline_value()
+
+    if IS_PLAYING:
+        ret, frame = VIDEO_CAPTURE.read()
+        if not ret:
+            VIDEO_CAPTURE.set(cv2.CAP_PROP_POS_FRAMES, 0)  # restart video
+            ret, frame = VIDEO_CAPTURE.read()
+    else:
+        VIDEO_CAPTURE.set(cv2.CAP_PROP_POS_FRAMES, VIDEO_CAPTURE.get(cv2.CAP_PROP_POS_FRAMES) - 1)  # repeat last frame
+        ret, frame = VIDEO_CAPTURE.read()
+    
+    # update timeline position
+    frame_number = int(VIDEO_CAPTURE.get(cv2.CAP_PROP_POS_FRAMES) - 1)
+    timeline_slider.set(frame_number)
+
+    read_zoom_from_controls()
+    read_speed_from_controls()
+
+    fly_paths = video_postprocess.construct_paths(PROCESSED_DATA, frame_number)
+    adjusted_frame = video_postprocess.copy_frame(frame)
+    video_postprocess.draw_paths_onto_frame(PROCESSED_DATA[frame_number], adjusted_frame, fly_paths)
+
+    # place adjusted and original frames side by side
+    height, width, _ = frame.shape
+    if width < height:
+        merged_frame = cv2.hconcat([adjusted_frame, frame])
+    else:
+        merged_frame = cv2.vconcat([adjusted_frame, frame])
+
+    # Convert the frame to a format that can be used by tk
+    img = Image.fromarray(cv2.cvtColor(merged_frame, cv2.COLOR_BGR2RGB))
+    resized = img.resize((int(img.width * ZOOM_SCALAR), int(img.height * ZOOM_SCALAR)), Image.LANCZOS)
+    imgtk = ImageTk.PhotoImage(image=resized)
+
+    # Update the image in the panel
+    preview_panel.imgtk = imgtk
+    preview_panel.config(image=imgtk)
+    
+    # Call this function again after some delay
+    ROOT_WINDOW.after(PLAYBACK_DELAY_MS, update_frame)
+
+def auto_process():
+    global PROCESSED_DATA, gap_entry
+    try:
+        gap = int(gap_entry.get())
+    except:
+        pass
+    PROCESSED_DATA = data_postprocess.process_data(STORED_RAW_DATA, gap)
+
+
+# read raw data from flytracker storage file
+PROCESSED_DATA = None
+STORED_RAW_DATA = storage_helper.read_binary_file(__INPUT_VIDEO)
+if STORED_RAW_DATA[0] == True:
+    STORED_RAW_DATA = STORED_RAW_DATA[1]
+    print('raw data loaded!')
+else:
+    print('could not load raw data!')
+    exit()
+
+# Initialize the video capture
+VIDEO_CAPTURE = cv2.VideoCapture(__INPUT_VIDEO)
+VIDEO_TOTAL_FRAMES = int(VIDEO_CAPTURE.get(cv2.CAP_PROP_FRAME_COUNT))
+IS_PLAYING = True
+ZOOM_SCALAR = 2.2
+PLAYBACK_DELAY_MS = int(1000 / VIDEO_CAPTURE.get(cv2.CAP_PROP_FPS))
+
+# Initialize tk window
+ROOT_WINDOW = tk.Tk()
+ROOT_WINDOW.title("Effect Preview")
+ROOT_WINDOW.state('zoomed')
+
+
+# Create the left and right frames
+LEFT_FRAME = tk.Frame(ROOT_WINDOW, bg=__BACKGROUND_LIGHT)
+LEFT_FRAME.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+RIGHT_FRAME = tk.Frame(ROOT_WINDOW, bg=__BACKGROUND_DARK)
+RIGHT_FRAME.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+# Create controls frame for preview panel
+PREVIEW_FRAME = tk.Frame(RIGHT_FRAME, bg=RIGHT_FRAME.cget('bg'))
+PREVIEW_FRAME.pack(fill=tk.BOTH, expand=True)
+
+# Add preview panel to the preview frame
+preview_panel = tk.Label(PREVIEW_FRAME, bg=PREVIEW_FRAME.cget('bg'))
+preview_panel.pack()
+
+# Add preview timeline
+timeline_slider = tk.Scale(PREVIEW_FRAME, from_=0, to=VIDEO_TOTAL_FRAMES, orient=tk.HORIZONTAL)
+timeline_slider.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=5)
+
+# Create two vertically stacked control frames inside the right frame with a separator
+PREVIEW_CONTROL_FRAME = tk.Frame(LEFT_FRAME, bg=LEFT_FRAME.cget('bg'))
+PREVIEW_CONTROL_FRAME.pack(fill=tk.BOTH, expand=False, padx=20, pady=20)
+
+CONTROL_SPACING_FRAME = tk.Frame(LEFT_FRAME, bg=__BACKGROUND_DARK, height=10)
+CONTROL_SPACING_FRAME.pack(fill=tk.X, expand=False)
+
+DATA_CONTROL_FRAME = tk.Frame(LEFT_FRAME, bg=LEFT_FRAME.cget('bg'))
+DATA_CONTROL_FRAME.pack(fill=tk.BOTH, expand=False, padx=20, pady=20)
+
+# Add buttons to control preview
+controls_label = ttk.Label(PREVIEW_CONTROL_FRAME, text='Preview controls:')
+controls_label.grid(row=0, column=0, padx=5, pady=5)
+
+play_preview_button = tk.Button(PREVIEW_CONTROL_FRAME, width=20, text="Play", command=resume_preview)
+play_preview_button.grid(row=0, column=1, padx=5, pady=5)
+
+pause_preview_button = tk.Button(PREVIEW_CONTROL_FRAME, width=20, text="Pause", command=pause_preview)
+pause_preview_button.grid(row=0, column=2, padx=5, pady=5)
+
+restart_preview_button = tk.Button(PREVIEW_CONTROL_FRAME, width=20, text="Restart", command=restart_preview)
+restart_preview_button.grid(row=0, column=3, padx=5, pady=5)
+
+# Add sliders to control preview
+zoom_label = ttk.Label(PREVIEW_CONTROL_FRAME, text='Zoom level [%]:')
+zoom_label.grid(row=1, column=0, sticky=tk.E, padx=5, pady=5)
+zoom_slider = tk.Scale(PREVIEW_CONTROL_FRAME, from_=50, to=250, orient=tk.HORIZONTAL, length=300)
+zoom_slider.set(ZOOM_SCALAR * 100)
+zoom_slider.grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
+
+speed_label = ttk.Label(PREVIEW_CONTROL_FRAME, text='Playback speed [%]:')
+speed_label.grid(row=1, column=2, sticky=tk.E, padx=5, pady=5)
+speed_slider = tk.Scale(PREVIEW_CONTROL_FRAME, from_=10, to=200, orient=tk.HORIZONTAL, length=300)
+speed_slider.set(100)
+speed_slider.grid(row=1, column=3, sticky=tk.W, padx=5, pady=5)
+
+
+# Add links control frame
+LINKS_FRAME = tk.Frame(DATA_CONTROL_FRAME, bg=DATA_CONTROL_FRAME.cget('bg'))
+LINKS_FRAME.grid(row=0, column=0, padx=10, pady=10)
+
+links_label = tk.Label(LINKS_FRAME, text='Links:')
+links_label.grid(row=0, column=0, columnspan=2, sticky=tk.W, pady=5)
+
+links_listbox = tk.Listbox(LINKS_FRAME)
+for i in range(10):
+    links_listbox.insert(tk.END, f'{(i+2)**2:<5} -> {i:<5}')
+links_listbox.grid(row=1, column=0, columnspan=2, sticky=tk.EW)
+
+
+gap_label = ttk.Label(LINKS_FRAME, text='Gap threshold [px]:')
+gap_label.grid(row=2, column=0, sticky=tk.EW, padx=5, pady=5)
+gap_entry = tk.Entry(LINKS_FRAME)
+gap_entry.insert(0, '3')
+gap_entry.grid(row=2, column=1, sticky=tk.W, padx=5, pady=5)
+
+auto_button = tk.Button(LINKS_FRAME, text="Automatically find links", command=auto_process)
+auto_button.grid(row=3, column=0, columnspan=2, sticky=tk.EW, padx=5, pady=5)
+
+
+# Add event listeners
+ROOT_WINDOW.bind("<Button-1>", deselect_all_entries)
+
+auto_process()
+# Start the frame update loop
+update_frame()
+
+# Start the tk event loop
+ROOT_WINDOW.mainloop()
+
+# Release the video capture object
+VIDEO_CAPTURE.release()

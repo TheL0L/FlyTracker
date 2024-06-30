dc03ea2295178ebd4cebaffe0b9c0aaa4cb6b188
Sergei <sergeiyakima1@gmail.com>
2024-06-30 20:49:09 +0300

    major refactoring and cleanup, separated functionality

--- app_v2.py
+++ app_v2.py
@@ -1,8 +1,8 @@
 from FlyTracker import FlyTracker
 import cv2, csv
 from tqdm import tqdm
-import helper
 from data_postprocess import process_data
+from video_postprocess import annotate_video
 import video_preprocess
 import file_helper
 
@@ -64,91 +64,6 @@ def analyze_video(fly_tracker, video_path, frame_preprocess_method = None):
     return data
 
 
-def annotate_video(data, video_path, output_path, constraints, draw_constraints = False):
-    # setup opencv video reader
-    stream = cv2.VideoCapture(video_path)
-    success, frame = stream.read()
-    height, width, _ = frame.shape
-
-    # setup opencv video writer
-    writer = cv2.VideoWriter(
-        output_path,
-        cv2.VideoWriter_fourcc(*'mp4v'),
-        int(stream.get(cv2.CAP_PROP_FPS)),
-        (width, height)
-    )
-
-    # setup progress bar
-    frames_count = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
-    progress_bar = tqdm(total=frames_count, desc='Annotation Progress', unit='frame')
-
-    # prepare a variable to hold the paths
-    paths = {}
-
-    while success:
-        # write a frame counter
-        cv2.putText(
-            img=        frame,
-            text=       f'{progress_bar.n}',
-            org=        (1, 15),
-            fontFace=   cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=  0.4,
-            color=      helper.rgb_to_bgr((255, 0, 0)),
-            thickness=  1,
-            lineType=   cv2.LINE_AA
-        )
-
-        # add current frame's points to the paths
-        for track in data[progress_bar.n]:
-            id, conf, x1, y1, x2, y2 = track
-
-            cv2.putText(
-                img=        frame,
-                text=       f'{id}',
-                org=        helper.make_point(*helper.get_center(x1,y1,x2,y2)),
-                fontFace=   cv2.FONT_HERSHEY_SIMPLEX,
-                fontScale=  0.4,
-                color=      helper.rgb_to_bgr(helper.id_to_color(id)),
-                thickness=  1,
-                lineType=   cv2.LINE_AA
-            )
-
-            if id not in paths:
-                paths[id] = []
-            paths[id].append(helper.get_center(x1, y1, x2, y2))
-
-        # draw paths constructed from points
-        for _id, _points in paths.items():
-            helper.draw_points_with_lines(
-                image=      frame,
-                points=     _points,
-                color=      helper.rgb_to_bgr(helper.id_to_color(_id))
-            )
-        
-        # draw constraints rectangle
-        if draw_constraints:
-            x1 = constraints['x_min'] if constraints['x_min'] is not None else 0
-            x2 = constraints['x_max'] if constraints['x_max'] is not None else width - 1
-            y1 = constraints['y_min'] if constraints['y_min'] is not None else 0
-            y2 = constraints['y_max'] if constraints['y_max'] is not None else height - 1
-            cv2.rectangle(
-                img=        frame,
-                pt1=        helper.make_point(x1, y1),
-                pt2=        helper.make_point(x2, y2),
-                color=      (0, 0, 255),
-                thickness=  1
-            )
-
-        writer.write(frame)
-
-        progress_bar.update()
-        success, frame = stream.read()
-    
-    progress_bar.close()
-    stream.release()
-    writer.release()
-
-
 def write_to_csv(data, output_path):
     with open(output_path, 'w', newline='') as file:
         writer = csv.writer(file)
@@ -156,6 +71,28 @@ def write_to_csv(data, output_path):
         for frame_num, tracks in data.items():
             for track in tracks:
                 writer.writerow([frame_num, *track])
+
+
+def process_video(ft: FlyTracker, video_path: str, output_root_path: str, prefix: str, suffix: str, preprocess_method) -> None:
+    # prepare output basename
+    file_name = file_helper.get_basename_stem(video_path)
+    file_name = f'{prefix}{file_name}{suffix}'
+    output_path = file_helper.join_paths(output_root_path, file_name)
+
+    # read and process data
+    raw_data = analyze_video(ft, video_path, preprocess_method)
+    processed_data = process_data(raw_data, max_tracks_gap=3)
+
+    # outputs
+    annotate_video(processed_data, video_path, f'{output_path}.mp4', ft.constraints, True)
+    write_to_csv(processed_data, f'{output_path}.csv')
+
+    # notify the user
+    print(f'results saved at:\n\t{output_path}.mp4\n\t{output_path}.csv\n')
+
+    # reset tracking for next video
+    ft.reset_tracking()
+    return
 
 
 
@@ -166,42 +103,26 @@ if __name__ == '__main__':
     output_root_path = file_helper.normalize_path('./showcase/result')
     file_helper.prepare_output_path(output_root_path)
 
-    _prefix = ''
-    _suffix = '_result'
+    prefix = ''
+    suffix = '_result'
 
     # setup model
     ft = FlyTracker('./runs/detect/train7/weights/best.pt')
     ft.set_constraints(y_min=100)
 
     # define a preprocess method for the videos
-    _preprocess_method = None
+    preprocess_method = None
 
     # iterate over videos
     for video_path in video_paths:
         # check video existance
         video_path = file_helper.normalize_path(video_path)
         if file_helper.check_existance(video_path):
-            print(f'working on {video_path}...')
+            print(f'working on "{video_path}"...')
         else:
             print(f'could not locate the video at "{video_path}".')
             continue
-
-        # prepare output basename
-        file_name = file_helper.get_basename_stem(video_path)
-        file_name = f'{_prefix}{file_name}{_suffix}'
-        output_path = file_helper.join_paths(output_root_path, file_name)
-
-        # read and process data
-        raw_data = analyze_video(ft, video_path, _preprocess_method)
-        processed_data = process_data(raw_data, max_tracks_gap=3)
-
-        # outputs
-        annotate_video(processed_data, video_path, f'{output_path}.mp4', ft.constraints, True)
-        write_to_csv(processed_data, f'{output_path}.csv')
-
-        # notify the user
-        print(f'results saved at:\n\t{output_path}.mp4\n\t{output_path}.csv\n')
-
-        # reset tracking for next video
-        ft.reset_tracking()
+        
+        process_video(ft, video_path, output_root_path, prefix, suffix, preprocess_method)
+        
     
---
+++ video_postprocess.py
@@ -0,0 +1,110 @@
+import helper
+import cv2
+from tqdm import tqdm
+import numpy as np
+
+def copy_frame(frame):
+    return np.copy(frame)
+
+def construct_paths(data, frame_number, paths = None):
+    # construct paths from data
+    if paths is None:
+        paths = {}
+        for f in range(frame_number+1):
+            for track in data[f]:
+                id, conf, x1, y1, x2, y2 = track
+                if id not in paths:
+                    paths[id] = []
+                paths[id].append(helper.get_center(x1, y1, x2, y2))
+
+def draw_paths_onto_frame(frame_data, frame, paths = None):
+    # annotate active paths with corresponding IDs
+    for track in frame_data:
+        id, conf, x1, y1, x2, y2 = track
+        cv2.putText(
+            img=        frame,
+            text=       f'{id}',
+            org=        helper.make_point(*helper.get_center(x1,y1,x2,y2)),
+            fontFace=   cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=  0.4,
+            color=      helper.rgb_to_bgr(helper.id_to_color(id)),
+            thickness=  1,
+            lineType=   cv2.LINE_AA
+        )
+
+    # draw paths constructed from points
+    for _id, _points in paths.items():
+        helper.draw_points_with_lines(
+            image=      frame,
+            points=     _points,
+            color=      helper.rgb_to_bgr(helper.id_to_color(_id))
+        )
+
+def draw_constraints_onto_frame(frame, constraints):
+    # get frame shape
+    width, height, _ = frame.shape
+    # draw constraints rectangle
+    x1 = constraints['x_min'] if constraints['x_min'] is not None else 0
+    x2 = constraints['x_max'] if constraints['x_max'] is not None else width - 1
+    y1 = constraints['y_min'] if constraints['y_min'] is not None else 0
+    y2 = constraints['y_max'] if constraints['y_max'] is not None else height - 1
+    cv2.rectangle(
+        img=        frame,
+        pt1=        helper.make_point(x1, y1),
+        pt2=        helper.make_point(x2, y2),
+        color=      (0, 0, 255),
+        thickness=  1
+    )
+
+def annotate_video(data, video_path, output_path, constraints, draw_constraints = False):
+    # setup opencv video reader
+    stream = cv2.VideoCapture(video_path)
+    success, frame = stream.read()
+    height, width, _ = frame.shape
+
+    # setup opencv video writer
+    writer = cv2.VideoWriter(
+        output_path,
+        cv2.VideoWriter_fourcc(*'mp4v'),
+        int(stream.get(cv2.CAP_PROP_FPS)),
+        (width, height)
+    )
+
+    # setup progress bar
+    frames_count = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
+    progress_bar = tqdm(total=frames_count, desc='Annotation Progress', unit='frame')
+
+    # prepare a variable to hold the paths
+    paths = {}
+
+    while success:
+        # get current frame number
+        frame_number = progress_bar.n
+
+        construct_paths(data, frame_number, paths)
+
+        # draw a frame counter
+        cv2.putText(
+            img=        frame,
+            text=       f'{frame_number}',
+            org=        (1, 15),
+            fontFace=   cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=  0.4,
+            color=      helper.rgb_to_bgr((255, 0, 0)),
+            thickness=  1,
+            lineType=   cv2.LINE_AA
+        )
+
+        draw_paths_onto_frame(data[frame_number], frame, paths)
+        
+        if draw_constraints == True:
+            draw_constraints_onto_frame(frame, constraints)
+
+        writer.write(frame)
+
+        progress_bar.update()
+        success, frame = stream.read()
+    
+    progress_bar.close()
+    stream.release()
+    writer.release()

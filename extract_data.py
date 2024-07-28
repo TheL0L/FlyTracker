b67f4e9a0a256dbbab5d23b1308fc73d4ca8f23a
Sergei <sergeiyakima1@gmail.com>
2024-07-28 20:04:07 +0300

    changed keys, added missing value

--- extract_data.py
+++ extract_data.py
@@ -5,13 +5,16 @@ import re
 def decompose_path(path: str) -> dict:
     norm_path = file_helper.normalize_path(path)
 
+    directory, filename, extension = file_helper.split_path(path)
+
     result = {
-        'age':                  None,
-        'mating date':          None,
-        'testing date':         None,
-        'group':                None,
-        'technical repetition': None,
-        'vial':                 None,
+        'Video Name':           filename + extension,
+        'Age':                  None,
+        'Mating Date':          None,
+        'Testing Date':         None,
+        'Group':                None,
+        'Technical Repetition': None,
+        'Vial Number':          None,
     }
     regex = re.compile(r'.*NGT-SCE/NGT_mating_\d+\.\d+\.\d+/(\d+)d(\d{8})_(\d{8})_NGT/.+/(\d+)_\d{8}_1\.(\d+)_start_v([1-5])\.avi$')
 
@@ -22,12 +25,12 @@ def decompose_path(path: str) -> dict:
     date_format = lambda date: f'{date[:2]}/{date[2:4]}/{date[4:]}'
 
     g = m.groups()
-    result['age']                  = int(g[0])
-    result['mating date']          = date_format(g[1])
-    result['testing date']         = date_format(g[2])
-    result['group']                = int(g[3])
-    result['technical repetition'] = int(g[4])
-    result['vial']                 = int(g[5])
+    result['Age']                  = int(g[0])
+    result['Mating Date']          = date_format(g[1])
+    result['Testing Date']         = date_format(g[2])
+    result['Group']                = int(g[3])
+    result['Technical Repetition'] = int(g[4])
+    result['Vial Number']          = int(g[5])
 
     return result
 

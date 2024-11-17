af53562c0bc94b40e62a937c671c3da8099c994b
Sergei <sergeiyakima1@gmail.com>
2024-11-18 00:41:17 +0200

    minor gui improvements

--- AdjustmentDialog.py
+++ AdjustmentDialog.py
@@ -12,7 +12,7 @@ class AdjustmentDialog(QDialog):
 
         self.current_frame = 1
         self.dragging = False
-        self.drag_margin = 15
+        self.drag_margin = 20
         self.hide_gui = False
         self.scale = 2
 
@@ -80,10 +80,31 @@ class AdjustmentDialog(QDialog):
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawPixmap(self.rect(), self.getScaledFrame(self.current_frame))
+        self.drawFrameNumber(painter)
         
         if not self.hide_gui:
             self.draw_path(painter)
             self.draw_points(painter)
+    
+    def drawFrameNumber(self, painter: QPainter):
+        font = self.font()
+        font.setPointSize(font.pointSize() + 6)
+        painter.setFont(font)
+
+        margin = 10
+        padding = 5
+        text = f'+{self.current_frame:0>{int(len(self.points) / 10) + 1}}'
+
+        text_rect = self.rect().adjusted(margin, margin, -margin, -margin)
+        background_rect = painter.boundingRect(text_rect, Qt.AlignLeft | Qt.AlignTop, text)
+        background_rect = background_rect.adjusted(-padding, -padding, padding, padding)
+
+        painter.setBrush(Qt.white)
+        painter.setPen(Qt.NoPen)
+        painter.drawRect(background_rect)
+
+        painter.setPen(QColor(0, 0, 255))
+        painter.drawText(text_rect, Qt.AlignLeft | Qt.AlignTop, text)
 
     def draw_path(self, painter: QPainter) -> None:
         painter.setPen(QPen(QColor(255, 0, 0), 2))
@@ -94,18 +115,18 @@ class AdjustmentDialog(QDialog):
             last = current
 
     def draw_points(self, painter: QPainter) -> None:
-        painter.setPen(QPen())
+        painter.setPen(QColor(0, 0, 0, 127))
         for x in range(len(self.points)):
             point = self.getScaledPoint(self.points[x])
             if x in [0, len(self.points)-1]:
-                painter.setBrush(QColor(0, 255, 0, 20))
-                painter.drawEllipse(point, 5, 5)
+                painter.setBrush(QColor(0, 255, 0, 63))
+                painter.drawEllipse(point, 8, 8)
             elif x == self.current_frame:
                 painter.setBrush(QColor(255, 0, 0, 127))
                 painter.drawEllipse(point, self.drag_margin, self.drag_margin)
             else:
-                painter.setBrush(QColor(255, 0, 0, 20))
-                painter.drawEllipse(point, 5, 5)
+                painter.setBrush(QColor(255, 0, 0, 63))
+                painter.drawEllipse(point, 8, 8)
 
     def get_points(self) -> list[tuple[int, int]]:
         return self.points
